b75f4860a783bb14c399394601a6fbaa9eadd7d6
StanMolveau <neostan49@gmail.com>
2018-03-26 16:37:13 +0200

    Added a cool front-end
    Added HTML / CSS / JS to the project to have a beautiful functional platform

---
+++ static/assets/js/script.js
@@ -0,0 +1,57 @@
+$(function(){
+
+	var body = $('body'),
+		stage = $('#stage'),
+		back = $('a.back');
+
+	/* Step 1 */
+
+	$('#step1 .encrypt').click(function(){
+		body.attr('class', 'encrypt');
+
+		// Go to step 2
+		step(2);
+	});
+
+	$('#step1 .decrypt').click(function(){
+		body.attr('class', 'decrypt');
+		step(2);
+	});
+
+
+	/* The back button */
+
+
+	back.click(function(){
+
+		// Reinitialize the hidden file inputs,
+		// so that they don't hold the selection 
+		// from last time
+
+		$('#step2 input[type=file]').replaceWith(function(){
+			return $(this).clone();
+		});
+
+		step(1);
+	});
+
+
+	// Helper function that moves the viewport to the correct step div
+
+	function step(i){
+
+		if(i == 1){
+			back.fadeOut();
+		}
+		else{
+			back.fadeIn();
+		}
+
+		// Move the #stage div. Changing the top property will trigger
+		// a css transition on the element. i-1 because we want the
+		// steps to start from 1:
+
+		stage.css('top',(-(i-1)*100)+'%');
+	}
+
+});
